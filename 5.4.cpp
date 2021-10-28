5a2ab8f8fad740b32063cb5ba213ef17f72181bc
Omar228228 <goblock542@gmail.com>
2021-10-28 22:40:48 +0300

    first commit

---
+++ 5.4.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+double P0(const int N, const int K)
+{
+	double n = 1;
+	for (int i = N; i <= K; i++)
+		n *= (1. * i * N) / ((1. * i * i) + (1. * N * N));
+	return n;
+}
+double P1(const int N, const int K, const int i)
+{
+	if (i > K)
+		return 1;
+	else
+		return (1. * i * N) / (1. * i * i + 1. * N * N) * P1(N, K, i + 1);
+}
+double P2(const int N, const int K, const int i)
+{
+	if (i < N)
+		return 1;
+	else
+		return (1. * i * N) / (1. * i * i + 1. * N * N) * P2(N, K, i - 1);
+}
+double P3(const int N, const int K, const int i, double t)
+{
+	t = t * (1. * i * N) / (1. * i * i + 1. * N * N);
+	if (i >= K)
+		return t;
+	else
+		return P3(N, K, i + 1, t);
+}
+double P4(const int N, const int K, const int i, double t)
+{
+	t = t * (1. * i * N) / (1. * i * i + 1. * N * N);
+	if (i <= N)
+		return t;
+	else
+		return P4(N, K, i - 1, t);
+}
+
+int main()
+{
+	int N;
+	int K = 16;
+	cout << "N = "; cin >> N;
+	cout << "(iter) P0 = " << P0(N, K) << endl;
+	cout << "(rec up ++) P1 = " << P1(N, K, N) << endl;
+	cout << "(rec up --) P2 = " << P2(N, K, K) << endl;
+	cout << "(rec down ++) P3 = " << P3(N, K, N, 1) << endl;
+	cout << "(rec down --) P4 = " << P4(N, K, K, 1) << endl;
+	return 0;
+}
---
+++ UnitTest5.4/UnitTest5.4.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest54
+{
+	TEST_CLASS(UnitTest54)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int N = 1, i = 0, K = 16;
+			int n;
+			n = (1. * i * N) / ((1. * i * i) + (1. * N * N));
+			Assert::AreEqual(n, 0);
+		}
+	};
+}
